863c006263daa89af5f2ee10cd8121d6e3ebe872
Soufiane Echkem <Soufiane.Echkem@gmail.com>
2024-03-08 02:21:29 +0100

    timer 4 it work

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -222,9 +222,9 @@ static void MX_TIM4_Init(void)
 
   /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 144-1;
+  htim4.Init.Prescaler = 0;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 10000;
+  htim4.Init.Period = 36000;
   htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
@@ -382,7 +382,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
   if (htim->Instance == TIM4) {
 	  interruptCount++;
 	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
-	  if (interruptCount == 200){
+	  if (interruptCount == 10){
 	        HAL_TIM_Base_Stop(&htim4);
 	        interruptCount = 0;
 	  }
